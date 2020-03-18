a7c6361471971aedb8139f75c317f6c098ef566f
Daliiuss <46731380+Daliiuss@users.noreply.github.com>
2020-03-18 20:47:52 +0200

    Second bullet point
    calculates average
    prints out with average

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Student
+    {
+        string name = string.Empty;
+        string lastname = string.Empty;
+        List<double> grades = new List<double>();
+        double exam = 0;
+        double avrg = 0;
+
+        public void GetStudentDetails(string inname, string inlastname, List<double> ingrades, double inexam)
+        {
+            name = inname;
+            lastname = inlastname;
+            grades = ingrades;
+            exam = inexam;
+            avrg=CalcAvrg(grades, avrg);
+        }
+        public double CalcAvrg(List<double> grades, double avrg)
+        {
+            return avrg = grades.Average();
+        }
+        public void PrintToConsoleStudentDetails()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(lastname);
+            Console.WriteLine(exam);
+        }
+        public void PrintToConsoleStudentAverage()
+        {
+            Console.WriteLine(String.Format("{0,-15} {1,-15} {2,5}", name, lastname, avrg));
+        }
+    }
+    class Program
+    {
+        static List<Student> allstudents = new List<Student>();
+        public static void EnterStudents(int studentcount)
+        {
+            Student tempstudent = new Student();
+            string inname, inlastname;
+            List<double> ingrades = new List<double>();
+            double inexam = 0;
+
+            for (int i = 0; i < studentcount; i++)
+            {
+                Console.WriteLine("enter name");
+                inname = Console.ReadLine();
+                Console.WriteLine("enter lastname");
+                inlastname = Console.ReadLine();
+                Console.WriteLine("enter grades");
+                for (int j = 0; j < 2; j++)
+                {
+                    ingrades.Add(Convert.ToDouble(Console.ReadLine()));
+                }
+                Console.WriteLine("enter exam");
+                inexam = Convert.ToDouble(Console.ReadLine());
+                tempstudent.GetStudentDetails(inname, inlastname, ingrades, inexam);
+                Program.allstudents.Add(tempstudent);
+                tempstudent = new Student();
+                ingrades.Clear();
+            }
+
+        }
+        public static void MENU()
+        {
+            string flag = " ";
+            string caseSwitch = "";
+            while (flag != "TERMINATE")
+            {
+                Console.WriteLine("1 - enter new students");
+                Console.WriteLine("2 - get student list with averages");
+                Console.WriteLine("TERMINATE - exit");
+                caseSwitch = Console.ReadLine();
+                switch (caseSwitch)
+                {
+                    case "1":
+                        Console.WriteLine("enter student count ");
+                        int studentcount = Convert.ToInt32(Console.ReadLine());
+                        EnterStudents(studentcount);
+                        break;
+                    case "2":
+                        Console.WriteLine("Vardas     Pavardė        Galutinis(Vid.)");
+                        Console.WriteLine("-----------------------------------------");
+                        for (int i = 0; i < allstudents.Count(); i++)
+                        {                          
+                            allstudents[i].PrintToConsoleStudentAverage();
+                        }
+                        
+                        break;
+                    case "TERMINATE":
+                        flag = "TERMINATE";
+                        return;
+                    default:
+                        break;
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            MENU();
+
+        }
+    }
+}
